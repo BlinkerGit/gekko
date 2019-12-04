f209206920da13170048ed2d150c6835b6509739
Jimmy Bartolotta <username@users.noreply.github.com>
2019-12-04 16:20:55 -0700

    Adding interest_rate to api.

--- lib/rates.ex
+++ lib/rates.ex
@@ -2,4 +2,5 @@ defmodule Rates do
   alias Gekko.Rates.Solver
 
   defdelegate apr(annual_rate, term, principal, fee), to: Solver
+  defdelegate interest_rate(payment, term, principal, guess), to: Solver
 end
